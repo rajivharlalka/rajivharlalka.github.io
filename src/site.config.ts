e6e92e399aed7a7c5ce511bf63aeca091d3eeddf
Rajiv Harlalka <rajivharlalka009@gmail.com>
2025-09-14 22:37:27 +0530

    add now page

--- src/site.config.ts
+++ src/site.config.ts
@@ -39,6 +39,10 @@ export const menuLinks: { path: string; title: string }[] = [
     path: '/posts/',
     title: 'Posts',
   },
+  {
+    path: '/now/',
+    title: 'Now',
+  },
 ]
 
 // https://expressive-code.com/reference/configuration/
